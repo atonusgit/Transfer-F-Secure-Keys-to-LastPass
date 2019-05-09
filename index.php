6a468b7c1ca0196efc3a5ca6f295a295d1fbbc62
Anton Valle <anton@atonus-black.local>
2019-05-09 15:06:08 +0300

    filepaths more clear

--- index.php
+++ index.php
@@ -1,18 +1,24 @@
 <?php
+	/**
+	 *	File paths
+	 *
+	 *	EDIT THESE
+	 */
+	$fs_filepath = '/Users/[[ USER ]]/Desktop/TallennetutSalasanat.fsk';
+	$lp_filepath = '/Users/[[ USER ]]/Desktop/file_for_lastpass_import.csv';
 
 	/**
 	 *	Import F-Secure Key file
 	 */
-	$fs_key_file = fopen("/Users/[[ USER ]]/Desktop/TallennetutSalasanat.fsk", "r");
-	$fs_key_json = fread($fs_key_file, filesize("/Users/[[ USER ]]/Desktop/TallennetutSalasanat.fsk"));
+	$fs_key_file = fopen($fs_filepath, "r");
+	$fs_key_json = fread($fs_key_file, filesize($fs_filepath));
 	fclose($fs_key_file);
 
 	/**
 	 *	Set up LastPass file
 	 */
 	$fs_key_array = json_decode($fs_key_json, true);
-	$lp_filename = '/Users/[[ USER ]]/Desktop/file_for_lastpass_import.csv';
-	$lp_file = fopen($lp_filename, 'w');
+	$lp_file = fopen($lp_filepath, 'w');
 
 	/**
 	 *	Create header row
